0948a3cec7bb944310bb971414af5eacf3a7f746
username <wecp-dummy@gmail.com>
2025-02-08 12:22:58 +0000

    event registerateion and files implemented

--- server/src/main/java/com/wecp/educationalresourcedistributionsystem/controller/RegisterAndLoginController.java
+++ server/src/main/java/com/wecp/educationalresourcedistributionsystem/controller/RegisterAndLoginController.java
@@ -22,14 +22,14 @@ import org.springframework.web.server.ResponseStatusException;
 public class RegisterAndLoginController {
 
 
-    // @PostMapping("/api/user/register")
-    // public ResponseEntity<User> registerUser(@RequestBody User user) {
-    //     // register the user and return the registered user with status code 201 CREATED.
-    // }
+    @PostMapping("/api/user/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        // register the user and return the registered user with status code 201 CREATED.
+    }
 
-    // @PostMapping("/api/user/login")
-    // public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
-    //     // login user and return the login response with status code 200 ok
-    //     // if authentication fails, return status code 401 unauthorized
-    // }
+    @PostMapping("/api/user/login")
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        // login user and return the login response with status code 200 ok
+        // if authentication fails, return status code 401 unauthorized
+    }
 }
