3e14121f81689c0dc052754e0e86af15a7daf948
username <wecp-dummy@gmail.com>
2025-02-08 09:06:53 +0000

    change in EventRegistration.java

--- server/src/main/java/com/wecp/educationalresourcedistributionsystem/entity/EventRegistration.java
+++ server/src/main/java/com/wecp/educationalresourcedistributionsystem/entity/EventRegistration.java
@@ -4,8 +4,17 @@ package com.wecp.educationalresourcedistributionsystem.entity;
 import javax.persistence.*;
 
 @Table(name = "registrations") // do not change table name
+@Entity
 public class EventRegistration {
 
    // implement entity
-}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private Long studentId;
+
+// adding my code sravani
+
+}
